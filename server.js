6b92a6c606c13303c7bbddbb2db0022e6afcb67e
Olivier Monnier <omonnier.dev@gmail.com>
2017-09-04 09:37:52 +0200

    in progress

--- server.js
+++ server.js
@@ -35,17 +35,7 @@ io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 
-  socket.on('iceCandidate', (message) => {
-    socket.broadcast.to(socket.id).emit('message', message);
-  });
-
-  socket.on('localDescription', (message) => {
-    socket.broadcast.to(socket.id).emit('message', message);
-  });
-
   socket.on('message', (message) => {
     socket.broadcast.to(socket.id).emit('message', message);
   });
 });
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
